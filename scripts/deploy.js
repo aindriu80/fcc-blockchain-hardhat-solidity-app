90d4c4aade7306dd80321f0d4b01a8a618594a62
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-08-21 14:15:57 +0100

    Programmatic Verification

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,5 @@
 // imports
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 // async main
 async function main() {
@@ -12,6 +12,27 @@ async function main() {
     // what's the private key?
     // what's the rpc url?
     console.log(`Deployed contract to: ${simpleStorage.address}`)
+    if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deployTransaction.wait(6)
+        await verify(simpleStorage.address, [])
+    }
+}
+
+// verify
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (err) {
+        if (err.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified")
+        } else {
+            console.log(err)
+        }
+    }
 }
 
 // main
